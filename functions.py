25bce390be8d93625d4e3392104d71dd2e20a0c1
Adham9 <60254901+Adham9@users.noreply.github.com>
2021-11-03 13:10:56 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -27,3 +27,28 @@ def chunkify(filepath, chunk_size, outputname):
         os.remove(output)
 
         batch_no += 1
+        
+        
+        
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]    
+
+
+
+def get_elon_quotes():
+
+    cd = 'Data\\' # Set working directory
+    filenames = find_csv_filenames(cd) #Get filenames ending wih csv
+    file_arr = np.array(filenames) # change list to numpy array
+    N =len(filenames) #Number of chunks
+    df1 = pd.read_csv("Data/" + file_arr[0]) # load first chunk
+    df_all = df1[df1["speaker"]=='Elon Musk'] # Extract elon musk quotes from first chunk file
+    # For loop through all chunks and concat data frames to have one data frame with all elon musk quotes
+    for ii in range(1,N):
+        name_2load = "Data/" + file_arr[ii]
+        current_df = pd.read_csv(name_2load)
+        df_elo_current = current_df[current_df["speaker"]=='Elon Musk']
+        df_all = pd.concat([df_all, df_elo_current], axis=0)
+    return df_all
+    
