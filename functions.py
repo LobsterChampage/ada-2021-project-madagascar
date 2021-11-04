25ab4a352f0ff2e3ed4a1dd8908806995a828072
LobsterChampagne <ferdinand.ytteborg@epfl.ch>
2021-11-04 23:54:28 +0100

    generalize get_quotes and add timing to chunkify

--- functions.py
+++ functions.py
@@ -2,8 +2,9 @@ import pandas as pd
 import bz2
 import os
 import numpy as np
+import time
 
-def chunkify(filepath, chunk_size, outputname):
+def chunkify(filepath, chunk_size, outputname, timing=False):
     """
     takes in the filepath as input
     takes in int as chunk_size
@@ -11,6 +12,10 @@ def chunkify(filepath, chunk_size, outputname):
     """
     batch_no = 1
     for chunk in pd.read_json(filepath, chunksize=chunk_size, lines=True, compression='bz2'):
+        # Taking the time of loading each chunk
+        if timing:
+            before = time.time()
+
         output = 'Data/' + outputname + str(batch_no) + '.csv'
 
         chunk.to_csv(output, index=False)
@@ -31,23 +36,31 @@ def chunkify(filepath, chunk_size, outputname):
         # Removes the .csv file to save space
         os.remove(output)
 
+        if timing:
+            after = time.time()
+            print(after - before, 's')
+
         batch_no += 1
 
 def find_csv_filenames(path_to_dir, year):
     filenames = os.listdir(path_to_dir)
     return [filename for filename in filenames if filename.startswith("quotes-" + str(year) + "-")]    
 
-def get_elon_quotes(year):
+def get_quotes(speaker, year):
     cd = 'Data/' # Set working directory
     filenames = find_csv_filenames(cd, year) #Get chunks from a given year
     file_arr = np.array(filenames) # change list to numpy array
     N = len(filenames) #Number of chunks
     df1 = pd.read_csv(cd + file_arr[0]) # load first chunk
-    df_all = df1[df1["speaker"]=='Elon Musk'] # Extract elon musk quotes from first chunk file
+    df_all = df1[df1["speaker"]==speaker] # Extract elon musk quotes from first chunk file
     # For loop through all chunks and concat data frames to have one data frame with all elon musk quotes
     for i in range(1,N):
         name_2load = cd + file_arr[i]
         current_df = pd.read_csv(name_2load)
-        df_elo_current = current_df[current_df["speaker"]=='Elon Musk']
+        df_elo_current = current_df[current_df["speaker"]==speaker]
         df_all = pd.concat([df_all, df_elo_current], axis=0)
     return df_all
+
+def make_csv(dataFrame, speaker, year, compression='bz2'):
+    dataFrame.to_csv('Data/' + speaker + '_quotes-' + str(year) + '.csv.' + compression)
+
